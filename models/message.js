246cfcde56ee5ba9f2badc178bfa0959fe2a4ab1
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2024-03-07 15:38:10 +0100

    update messages

--- models/message.js
+++ models/message.js
@@ -1,30 +1,25 @@
 import mongoose, { mongo } from 'mongoose';
 
-const messageSchema = new mongoose.Schema(
-	{
-		user: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-		},
-		courseId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Course',
-		},
-		messageType: {
-			type: String,
-			enum: ['text'],
-		},
-		text: String,
-		imageUrl: String,
-		createdAt: {
-			type: Date,
-			default: Date.now,
-		},
+const messageSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
 	},
-	{
-		timestamps: true,
+	courseId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Course',
 	},
-);
+	messageType: {
+		type: String,
+		enum: ['text'],
+	},
+	text: String,
+	imageUrl: String,
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	},
+});
 
 const Message = mongoose.model('Message', messageSchema);
 
